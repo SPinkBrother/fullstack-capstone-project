fdc09b7b94b4b68cbcc45d2444c457a07f2cc332
SPinkBrother <spinkbrotherofficial@gmail.com>
2026-07-12 23:51:41 -0400

    Implement Back-end in Login Component

--- giftlink-frontend/src/components/LoginPage/LoginPage.js
+++ giftlink-frontend/src/components/LoginPage/LoginPage.js
@@ -2,16 +2,71 @@
 import React, { useState } from 'react';
 import './LoginPage.css';
 
+import {urlConfig} from '../../config';
+import { useAppContext } from '../../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
+
+
 function LoginPage() {
 
     //insert code here to create useState hook variables for email, password
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [incorrect, setIncorrect] = useState('');
+
+
+    const navigate = useNavigate();
+    const bearerToken = sessionStorage.getItem('bearer-token');
+    const { setIsLoggedIn } = useAppContext();
+
+    useEffect(() => {
+        if (sessionStorage.getItem('auth-token')) {
+            navigate('/app')
+        }
+        }, [navigate])
 
     // insert code here to create handleLogin function and include console.log
 
-    const handleLogin = async(e) => {e.preventDefault();}
-		return (
+    const handleLogin = async(e) => {e.preventDefault();
+        //api call
+        const res = await fetch(`${urlConfig.backendUrl}/api/auth/login`, {
+			//Step 1 - Task 7
+			method: 'POST',
+			//Step 1 - Task 8
+          headers: {
+            'content-type': 'application/json',
+            'Authorization': bearerToken ? `Bearer ${bearerToken}` : '', // Include Bearer token if available
+        },
+		//Step 1 - Task 9
+          body: JSON.stringify({
+            email: email,
+            password: password,
+          })
+        });
+
+		//Step 2: Task 1
+		const json = await res.json();
+        console.log('Json',json);
+        if (json.authtoken) {
+			//Step 2: Task 2
+          sessionStorage.setItem('auth-token', json.authtoken);
+          sessionStorage.setItem('name', json.userName);
+          sessionStorage.setItem('email', json.userEmail);
+			//Step 2: Task 3
+          setIsLoggedIn(true);
+			//Step 2: Task 4
+	      navigate('/app');
+        } else {
+			//Step 2: Task 5
+          document.getElementById("email").value="";
+          document.getElementById("password").value="";
+          setIncorrect("Wrong password. Try again.");
+          setTimeout(() => {
+            setIncorrect("");
+          }, 2000);
+        }
+    }
+	    return (
       <div className="container mt-5">
         <div className="row justify-content-center">
           <div className="col-md-6 col-lg-4">
@@ -40,6 +95,8 @@ function LoginPage() {
                         value={email}
                         onChange={(e) => setEmail(e.target.value)}
                     />
+                    <span style={{color:'red',height:'.5cm',display:'block',fontStyle:'italic',fontSize:'12px'}}>{incorrect}</span>
+
                 </div>
 
   		{/* insert code here to create input elements for the variables email and  password */}
